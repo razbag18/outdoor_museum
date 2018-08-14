a87df5dc24970a69735cf998e0c868ca106e814d
Renae <renaejuvan@gmail.com>
2018-08-14 11:42:02 +1000

    created actual login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resource :sessions, only: [:new, :create, :destroy]
   resources :users_pictures
   resources :comments
   resources :pictures
   resources :users
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
