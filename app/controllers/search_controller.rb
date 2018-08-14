a14eafba9081eb1329de39cd15492663a5e702ba
micaelsbno <micaelsbno@gmail.com>
2018-08-14 12:06:17 +1000

    Added maps test

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,14 @@
 class SearchController < ApplicationController
 
   def index
+
+    # user search for street / or location
+    # request to geocode to get lat / long from st name
+
+    # map uluru is generated
     @google = HTTParty.get("https://maps.googleapis.com/maps/api/js?key=#{ENV['GOOGLE_MAPS_KEY']}&callback=initMap")
+    byebug
+    @uluru = gmaps.geocode(params[:search])
+
   end
 end
