a1485917288899628af37d6791673f87a5c03798
Renae <renaejuvan@gmail.com>
2018-08-15 09:57:26 +1000

    show pictures page and comments enabled

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+  def create
+    comment = Comment.new
+    comment.content = params[:content]
+    comment.picture_id = params[:picture_id]
+    comment.user_id = current_user.id
+    if comment.save
+      redirect_to "/pictures/#{ params[:picture_id]}"
+    else
+      render :index
+    end
+  end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,11 +3,20 @@ class SearchController < ApplicationController
   def index
     @pic = Picture.first
     pic_votes = @pic.users_pictures
-    @ratings = pic_votes.pluck('rating').reduce(:+)/pic_votes.length
+    total_points = pic_votes.pluck('rating').reduce(:+)
+    if total_points 
+      @ratings = total_points/pic_votes.length
+    else
+      @ratings = 0
+    end
+    
     @google_map_loader = google_script
     location = gmaps.geocode(params[:search])
     @lat = location[0][:geometry][:location][:lat]
     @lng = location[0][:geometry][:location][:lng]
   end
+
+  
   
 end
+
