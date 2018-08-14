6275a419db8b6dabf8a1d5a18d067da573ed36a2
micaelsbno <micaelsbno@gmail.com>
2018-08-14 14:44:20 +1000

    add seeds file

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,9 @@
 class SearchController < ApplicationController
 
   def index
+
+    # select all from database where radius === radius of the search
+    
     @google_map_loader = google_script
     location = gmaps.geocode(params[:search])
     @lat = location[0][:geometry][:location][:lat]
