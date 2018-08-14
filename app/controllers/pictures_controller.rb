f7990fb879a17abb38740db86f113081a1a33bb4
Lin Yang <lin.yang@rea-group.com>
2018-08-14 14:59:39 +1000

    save lantitude and longitude to db

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -7,6 +7,11 @@ class PicturesController < ApplicationController
   def create
     @picture = Picture.new(picture_params)
     @picture.user_id = current_user.id
+
+    result = gmaps.geocode( "#{picture_params[:street_name]}#{params[:picture][:street_name]}" )
+    @picture.latitude = result[0][:geometry][:location][:lat]
+    @picture.longitude = result[0][:geometry][:location][:lng]
+
     binding.pry
     if @picture.save
       redirect_to '/'
