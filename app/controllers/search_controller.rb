1dbb324affe3158c746d8ae7c9c97187797a303b
micaelsbno <micaelsbno@gmail.com>
2018-08-15 13:03:19 +1000

    add search radius function

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,27 +1,21 @@
 class SearchController < ApplicationController
 
   def index
-    @pic = Picture.first
-    pic_votes = @pic.users_pictures
-    # @ratings = pic_votes.pluck('rating').reduce(:+)/pic_votes.length
     @google_map_loader = google_script
     location = gmaps.geocode(params[:search])
     @lat = location[0][:geometry][:location][:lat]
     @lng = location[0][:geometry][:location][:lng]
-
-
-    @pictures = Picture.all
-
-
+    radius = (7000.to_f / 100000)
+    @pictures = 
+    Picture.all
+    .select{ |picture| 
+      (picture.latitude - @lat )**2 + (picture.longitude - @lng)**2 < radius**2
+    }
   end
   
 end
 
-# find all the picture locations
-# Picture.all.pluck geolocations
 
 
-
-# add all markers to map
-# add function to show pop up after you click
-# add marker to page (depending )
+# render markers again everytime user moves the map
+# 
