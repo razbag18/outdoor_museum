6b425620dfc6cebffa76ce5a0bd6295b4a7e3972
Renae <renaejuvan@gmail.com>
2018-08-15 17:00:07 +1000

    favorites works

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,8 @@ Rails.application.routes.draw do
 
   get '/home/userdashboard', to: 'homepage#user_dashboard'
 
-
-  
   post '/submit_rating', to: 'api/users_pictures#update_rating'
+  post '/toggle_favourites', to: 'api/users_pictures#toggle_favourites'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
