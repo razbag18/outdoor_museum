19b7caa98c6523c49412b9618734baf165c9f298
micaelsbno <micaelsbno@gmail.com>
2018-08-15 10:08:50 +1000

    added many markers

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,11 +3,25 @@ class SearchController < ApplicationController
   def index
     @pic = Picture.first
     pic_votes = @pic.users_pictures
-    @ratings = pic_votes.pluck('rating').reduce(:+)/pic_votes.length
+    # @ratings = pic_votes.pluck('rating').reduce(:+)/pic_votes.length
     @google_map_loader = google_script
     location = gmaps.geocode(params[:search])
     @lat = location[0][:geometry][:location][:lat]
     @lng = location[0][:geometry][:location][:lng]
+
+
+    @pictures = Picture.all
+
+
   end
   
 end
+
+# find all the picture locations
+# Picture.all.pluck geolocations
+
+
+
+# add all markers to map
+# add function to show pop up after you click
+# add marker to page (depending )
