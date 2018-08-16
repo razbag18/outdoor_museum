b1fe24e841af6960b310c12f31c842119b26fe14
Lin Yang <lin.yang@rea-group.com>
2018-08-16 11:19:20 +1000

    show different homepage for login user

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,12 +1,10 @@
 class HomepageController < ApplicationController
   def index
     @google_address_auto = google_auto_complete
-  end
-
-  def user_dashboard
-    show_user_uploaded_pics
-    show_user_favourited_pics
-    render :user_dashboard
+    if logged_in?
+      show_user_uploaded_pics
+      show_user_favourited_pics
+    end
   end
 
   def show_user_uploaded_pics
@@ -16,5 +14,4 @@ class HomepageController < ApplicationController
   def show_user_favourited_pics
     @user_favourited_pictures = UsersPicture.where(user_id: session[:user_id], isfav: true).joins(:picture).includes(:picture)
   end
-
 end
