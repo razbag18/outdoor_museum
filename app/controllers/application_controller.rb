214260e18169bd1d12abfc39cad80b919061a56e
micaelsbno <micaelsbno@gmail.com>
2018-08-17 11:44:34 +1000

    final commit (for now)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,11 @@ class ApplicationController < ActionController::Base
     @google_auto_complete = HTTParty.get("https://maps.googleapis.com/maps/api/js?key=#{ENV['GOOGLE_MAPS_KEY']}&callback=initMap&libraries=places")
   end
 
+  def google_auto_complete_menu
+    @google_auto_complete_menu = HTTParty.get("https://maps.googleapis.com/maps/api/js?key=#{ENV['GOOGLE_MAPS_KEY']}&callback=initMenuComplete&libraries=places")
+  end
+  helper_method :google_auto_complete_menu
+  
   include SessionsHelper
 
 end
