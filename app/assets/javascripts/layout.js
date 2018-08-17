ec1a3f9aa37bfa3f420fff5f1acb711e437e55c7
micaelsbno <micaelsbno@gmail.com>
2018-08-17 10:58:04 +1000

    maybe fixed menu mobile

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,10 +1,18 @@
 
 var menu = document.querySelector('.hidden-menu')
 
-document.addEventListener('click', ()=> {
-  if (event.target.classList.contains('fa-bars')) {
-    menu.className = "hidden-menu show"  
-  } else if (event.target.classList.contains('fa-times')) {
-    menu.className = "hidden-menu hide"
-  }
-})
+// document.querySelector('.menu').addEventListener('click', ()=> {
+//   if (event.target.classList.contains('fa-bars')) {
+//     menu.className = "hidden-menu show"  
+//   } else if (event.target.classList.contains('fa-times')) {
+//     menu.className = "hidden-menu hide"
+//   }
+// })
+
+function showMenu() {
+   menu.className = "hidden-menu show"
+}
+
+function hideMenu() {
+   menu.className = "hidden-menu hide"
+}
